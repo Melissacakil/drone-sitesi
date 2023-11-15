0f57072c5ff7090e930a2dafddcc404058de35ef
Melisa Cakil <melisacakil@gmail.com>
2023-11-15 16:28:15 +0300

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Cards from "../components/Card"; 
+import BasicAccordion from "../components/Accordion";
 
-
+import SwipeableTextMobileStepper from "../components/Carousel"
 
 const Home = () =>{
   return (
+    <>
     <Cards/>
+    <BasicAccordion/>
+    <SwipeableTextMobileStepper/>
+
+    </>
   );
 };
 
